37911f0ed12561ba5a18ae267214ccae09a14b89
dualface <dualface@gmail.com>
2016-05-06 12:50:11 +0800

    update WidgetComponent

--- creator_lua/src/creator/components/WidgetComponent.lua
+++ creator_lua/src/creator/components/WidgetComponent.lua
@@ -15,9 +15,14 @@ local _RIGHT      = 32
 local _TOP_BOTTOM = _TOP + _BOTTOM
 local _LEFT_RIGHT = _LEFT + _RIGHT
 
+local _glview
+
 function WidgetComponent:ctor(props)
     WidgetComponent.super.ctor(self)
     self.props = props
+    if not _glview then
+        _glview = cc.Director:getInstance():getOpenGLView()
+    end
 end
 
 function WidgetComponent:bind(target)
@@ -28,95 +33,103 @@ function WidgetComponent:onEnter(target)
 end
 
 function WidgetComponent:align(target)
-    local p = self.props
-    local flags = p._alignFlags
+    cc.printinfo("[WidgetComponent] align target %s [%s] %s", target.__type__, target.__id__, target:getName())
 
-    if flags == 0 then return end
 
-    if not target.parentref or not target.parentref[1] then return end
+    if self.props._alignFlags == 0
+            or not target.parentref
+            or not target.parentref[1] then
+        return
+    end
+
+    local props  = self.props
+    local flags  = props._alignFlags
     local parent = target.parentref[1]
 
     -- get parent content size
+    local pap    = parent:getAnchorPoint()
     local pw, ph = parent.contentSize.width, parent.contentSize.height
-    if parent.__type__ == "cc.Scene" then
-        local size = cc.Director:getInstance():getOpenGLView():getDesignResolutionSize()
-        pw, ph = size.width, size.height
-    end
-
-    local ap = parent:getAnchorPoint()
     local hw, hh = pw / 2, ph / 2
-    local cx = pw * (1 - ap.x) - hw
-    local cy = ph * (1 - ap.y) - hh
-    local left   = -hw
-    local right  = hw
-    local top    = hh
-    local bottom = -hh
+    -- cc.dump(parent.contentSize, "parent content size")
+    -- cc.dump(pap, "parent anchor point")
+
+    -- local cx     = pw * (1 - pap.x) - hw
+    -- local cy     = ph * (1 - pap.y) - hh
+    local cx, cy = 0, 0
+    -- cc.dump({cx = cx, cy = cy}, "parent center position")
+
+    local pleft   = -hw
+    local pright  = hw
+    local ptop    = hh
+    local pbottom = -hh
+
+    -- if parent.__type__ == "cc.Scene" then
+    --     local rect = _glview:getVisibleRect()
+    --     left   = rect.x
+    --     right  = left + rect.width
+    --     bottom = rect.y
+    --     top    = bottom + rect.height
+    --     pw     = right - left
+    --     ph     = top - bottom
+    --     hw     = pw / 2
+    --     hh     = ph / 2
+    --     cx     = left + pw / 2
+    --     cy     = bottom + ph / 2
+    -- end
 
     -- get target size
-    local cs = target.contentSize
-    local ap = target:getAnchorPoint()
-    local cw, ch
-    local x, y
+    local ap   = target:getAnchorPoint()
+    cc.printinfo("target ap: %0.2f, %0.2f", ap.x, ap.y)
 
-    -- check abs
-    local pleft = p._left
-    if not p._isAbsLeft then
-        pleft = pw * pleft
+    local w, h = target.contentSize.width, target.contentSize.height
+    local x, y = target:getPosition()
+
+    -- calc offsets
+    local left = props._left
+    if not props._isAbsLeft then
+        left = left * pw
     end
 
-    local pright = p._right
-    if not p._isAbsRight then
-        pright = pw * pright
+    local right = props._right
+    if not props._isAbsRight then
+        right = right * pw
     end
 
-    local ptop = p._top
-    if not p._isAbsTop then
-        ptop = ph * ptop
+    local top = props._top
+    if not props._isAbsTop then
+        top = top * ph
     end
 
-    local pbottom = p._bottom
-    if not p._isAbsBottom then
-        pbottom = pw * pbottom
+    local bottom = props._bottom
+    if not props._isAbsBottom then
+        bottom = bottom * ph
     end
 
-
+    -- calc position
     if bit_and(flags, _CENTER) ~= 0 then
-        x = cx
-    elseif bit_and(flags, _LEFT_RIGHT) == _LEFT_RIGHT then
-        cw = width - (pleft + pright)
-        x = left + pleft + cw * ap.x
+        x = cx + w * (ap.x - 0.5)
+    -- elseif bit_and(flags, _LEFT_RIGHT) == _LEFT_RIGHT then
+    --     cw = width - (pleft + pright)
+    --     x = left + pleft + cw * ap.x
     elseif bit_and(flags, _LEFT) ~= 0 then
-        x = left + pleft + cs.width * ap.x
+        x = pleft + left + w * ap.x
     elseif bit_and(flags, _RIGHT) ~= 0 then
-        x = right - pright - cs.width * (1 - ap.x)
+        x = pright - right - w * (1.0 - ap.x)
     end
 
     if bit_and(flags, _MID) ~= 0 then
-        y = cy
-    elseif bit_and(flags, _TOP_BOTTOM) == _TOP_BOTTOM then
-        ch = height - (pbottom + ptop)
-        y = _bottom + pbottom + ch * ap.y
+        y = cy + h * (ap.y - 0.5)
+    -- elseif bit_and(flags, _TOP_BOTTOM) == _TOP_BOTTOM then
+    --     ch = height - (pbottom + ptop)
+    --     y = _bottom + pbottom + ch * ap.y
     elseif bit_and(flags, _TOP) ~= 0 then
-        y = top - ptop - cs.height * (1 - ap.y)
+        y = ptop - top - h * (1.0 - ap.y)
     elseif bit_and(flags, _BOTTOM) ~= 0 then
-        y = bottom + pbottom + cs.height * ap.y
+        y = pbottom + bottom + h * ap.y
     end
 
-    if cw and ch then
-        target:setContentSize(cc.size(cw, ch))
-    elseif cw then
-        target:setContentSize(cc.size(cw, cs.height))
-    elseif ch then
-        target:setContentSize(cc.size(cs.width, ch))
-    end
-
-    if x and y then
-        target:setPosition(x, y)
-    elseif x then
-        target:setPositionX(x)
-    elseif y then
-        target:setPositionY(y)
-    end
+    cc.printinfo("target [%s] pos: %0.2f, %0.2f", target.__id__, x, y)
+    target:setPosition(x, y)
 end
 
 return WidgetComponent
